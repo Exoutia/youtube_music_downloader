14fff86568562c55a64797c0f3b184fd9fa07b50
exoutia <jhav365@gmail.com>
2024-04-04 14:43:19 +0530

    creating a simple thumbnail downloader using python
    It has three function which uses either search by string or by url but
    one thing is certain that both all of them is not able to download the
    whole image I don't know the reason and that's what i have to find out.

--- thumbnail_downloader.py
+++ thumbnail_downloader.py
@@ -7,7 +7,6 @@ from main import main
 
 def download_thumbnail(filename):
     search_video_res = pytube.Search(filename)
-    # print(search_video_res.results[0])
     video = search_video_res.results[0]
     thumbnail_url = video.thumbnail_url
     urllib.request.urlretrieve(thumbnail_url, filename.replace(" ", "_") + ".jpg")
@@ -23,11 +22,16 @@ def download_thumbnail_two(filename):
         with open(filename.replace(" ", "_") + "url_open.jpg", "wb") as f:
             f.write(data)
 
+
 def download_thumbnail_using_url(url, filename):
     video = pytube.YouTube(url)
     thumbnail_url = video.thumbnail_url
     print(thumbnail_url)
-    urllib.request.urlretrieve(thumbnail_url, filename.replace(" ", "_") + "using_link.jpg")
+    urllib.request.urlretrieve(
+        thumbnail_url, filename.replace(" ", "_") + "using_link.jpg"
+    )
+
+# TODO: Find the right method to download the thumbnail from youtube.
 
 if __name__ == "__main__":
     download_thumbnail_using_url("https://youtube.com/watch?v=WszNqHKwQbk", "bohoo")
