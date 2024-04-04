e0cc735c0555b08db26d2d7692f84aee460f3dff
exoutia <jhav365@gmail.com>
2024-04-04 14:40:29 +0530

    creating a simple thumbnail downloader using python
    It has three function which uses either search by string or by url but
    one thing is certain that both all of them is not able to download the
    whole image I don't know the reason and that's what i have to find out.

---
+++ thumbnail_downloader.py
@@ -0,0 +1,33 @@
+import urllib.request
+
+import pytube
+
+from main import main
+
+
+def download_thumbnail(filename):
+    search_video_res = pytube.Search(filename)
+    # print(search_video_res.results[0])
+    video = search_video_res.results[0]
+    thumbnail_url = video.thumbnail_url
+    urllib.request.urlretrieve(thumbnail_url, filename.replace(" ", "_") + ".jpg")
+
+
+def download_thumbnail_two(filename):
+    search = pytube.Search(filename)
+    video = search.results[0]
+    thumbnail_url = video.thumbnail_url
+    print(thumbnail_url)
+    with urllib.request.urlopen(thumbnail_url) as res:
+        data = res.read()
+        with open(filename.replace(" ", "_") + "url_open.jpg", "wb") as f:
+            f.write(data)
+
+def download_thumbnail_using_url(url, filename):
+    video = pytube.YouTube(url)
+    thumbnail_url = video.thumbnail_url
+    print(thumbnail_url)
+    urllib.request.urlretrieve(thumbnail_url, filename.replace(" ", "_") + "using_link.jpg")
+
+if __name__ == "__main__":
+    download_thumbnail_using_url("https://youtube.com/watch?v=WszNqHKwQbk", "bohoo")
