4befd4aa3768468362d819ea1294f6770f40580a
exoutia <jhav365@gmail.com>
2024-04-05 20:22:31 +0530

    creating the app factory using python flask

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # Create app and configure it based on test or other situation
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "app.sqlite")
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    if not os.path.exists(app.instance_path):
+        os.makedirs(app.instance_path)
+
+    @app.route("/hello")
+    def hello_world():
+        return "Hello world!"
+    
+    return app
